5f377fe0490b3cd5c0f6ae5b55b2a02669250366
FLAVIE <flavie@MacBook-Pro-de-FLAVIE.local>
2021-04-06 00:26:52 +0200

    JS_Exercice_P2_6

---
+++ main.js
@@ -0,0 +1,7 @@
+function changePics(image){
+   document.getElementById(image).src ="/Users/flavie/Desktop/formation/js/JS_Exercice_P2_6/images/" + image + "_2.jpg"
+}
+function returnPics(image){
+   document.getElementById(image).src ="/Users/flavie/Desktop/formation/js/JS_Exercice_P2_6/images/" + image + ".jpg"
+}
+
